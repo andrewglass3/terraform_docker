df23d8ed1ee1090bd519f2a4617800f433d2a52c
Andrew Glass <andrew.glass@outlook.com>
2020-12-11 11:58:00 +0000

    update push only

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "DockerDevBox" {
     ami = "ami-0aef57767f5404a3c"
     instance_type = "t3.micro"
     key_name = "terraform_dublin"
-    user_data = "${file("install_docker.sh")}"
+    user_data = file("install_docker.sh")
     security_groups = ["${aws_security_group.docker_dev_box.name}"]
     tags = {
         Name = "EC2_Instance_Terraform"
