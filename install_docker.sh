c9f9fbc858af603a5e34a7ce1dd37ad4b99aed5b
andrewglass3 <andrew.glass@outlook.com>
2020-12-11 11:06:38 +0000

    Update install_docker.sh
    changed silliness on line 10

--- install_docker.sh
+++ install_docker.sh
@@ -7,4 +7,4 @@ sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker ubuntu
-sudo systemctl restart
+sudo reboot
