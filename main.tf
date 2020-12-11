1c641f8738c40ed039de949c56b5ec4973fd0cd2
Andrew Glass <andrew.glass@outlook.com>
2020-12-11 19:48:26 +0000

    changed interpolation syntax

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "DockerDevBox" {
     instance_type = "t3.micro"
     key_name = "terraform_dublin"
     user_data = file("install_docker.sh")
-    security_groups = ["${aws_security_group.docker_dev_box.name}"]
+    security_groups = [aws_security_group.docker_dev_box.name]
     tags = {
         Name = "EC2_Instance_Terraform"
     }
