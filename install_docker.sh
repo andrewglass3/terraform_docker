d14dc4cfaad2e330ccc86f41925208c45b43b5bf
andrewglass3 <andrew.glass@outlook.com>
2020-12-11 10:46:07 +0000

    Update install_docker.sh
    Added docker-compose install

--- install_docker.sh
+++ install_docker.sh
@@ -4,4 +4,6 @@ sudo apt-get upgrade -y
 sudo apt-get install curl wget
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker ubuntu
