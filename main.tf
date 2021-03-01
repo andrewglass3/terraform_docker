9461e052b2c53fb55285ff3a9ac637ba6dbc6264
Andrew Glass <andrew.glass@outlook.com>
2021-03-01 10:05:21 +0000

    fixed typo on line 18 of main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state" {
     server_side_encryption_configuration {
         rule {
             apply_server_side_encryption_by_default {
-                sse_algorith = "AES256"
+                sse_algorithm = "AES256"
             }
         }
     }
